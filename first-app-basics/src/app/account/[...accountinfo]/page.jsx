8db723cefc63053179433e29861e174d34b4aadb
Raval Vandan <vandanraval2002@gmail.com>
2024-08-25 16:34:14 +0530

    implemented useRouter for navigation way-2 instead of using Link

--- first-app-basics/src/app/account/[...accountinfo]/page.jsx
+++ first-app-basics/src/app/account/[...accountinfo]/page.jsx
@@ -1,9 +1,22 @@
-const myprofile = ()=>{
-    return(
-        <div className="flex bg-yellow-300 min-h-screen w-full border border-black justify-center items-center">
-        <h1 className="text-5xl">This is My account info  page</h1>
-        </div>
-    )
-}
+"use client"
+import { useRouter } from "next/navigation";
+const myprofile = () => {
+  const router = useRouter()
 
-export default myprofile
+  function backtohome() {
+    router.back("");
+  }
+  return (
+    <div className="flex flex-col gap-9 bg-yellow-300 min-h-screen w-full justify-center items-center">
+      <h1 className="text-5xl">This is My account info page</h1>
+      <button
+        className="p-3 w-[150px] mt-8 bg-red-600 font-semibold h-[60px] block text-white"
+        onClick={backtohome}
+      >
+        Back to home
+      </button>
+    </div>
+  );
+};
+
+export default myprofile;
--- first-app-basics/src/app/page.js
+++ first-app-basics/src/app/page.js
@@ -1,17 +1,51 @@
-import Image from "next/image";
+"use client";
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col justify-center items-center  p-24">
-     <h1 className="text-5xl">welcome in my first app.....</h1>
-<div className="flex gap-5 mt-12">
+  const router = useRouter();
 
-  
-     <Link className="p-4 cursor-pointer  bg-blue-500 w-[170px] h-[60px] text-white font-bold" href={"/account/accountinfo"}>My Account Page</Link>
- 
-     <Link className="p-4 cursor-pointer  bg-blue-500 w-[170px] h-[60px] text-white font-bold" href={"/profile/details"}>My Profile Page</Link>
-     </div>
-      </main>
+  function navigatetoaccount() {
+    router.push('account/accountinfo');
+  }
+  function navigatetoprofile() {
+    router.push('profile/profileinfo');
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col  items-center  p-24">
+      <h1 className="text-5xl">welcome in my first app.....</h1>
+      <h2>Way 1 Of Navigation</h2>
+      <div className="flex gap-5 mt-12">
+        <Link
+          className="p-4 cursor-pointer  bg-blue-500 w-[170px] h-[60px] text-white font-bold"
+          href={"/account/accountinfo"}
+        >
+          My Account Page
+        </Link>
+
+        <Link
+          className="p-4 cursor-pointer  bg-blue-500 w-[170px] h-[60px] text-white font-bold"
+          href={"/profile/details"}
+        >
+          My Profile Page
+        </Link>
+      </div>
+      <h2 className="mt-5">Way 2 Of Navigation</h2>
+      <div className="flex gap-5 mt-12">
+        <buton
+          onClick={navigatetoaccount}
+          className="p-4 cursor-pointer  bg-green-500 w-[170px] h-[60px] text-white font-bold"
+        >
+          My Account Page
+        </buton>
+
+        <button
+          onClick={navigatetoprofile}
+          className="p-4 cursor-pointer  bg-green-500 w-[170px] h-[60px] text-white font-bold"
+        >
+          My Profile Page
+        </button>
+      </div>
+    </main>
   );
 }
